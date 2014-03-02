16e6efefb706cebf21dce11987ab98092a5658f1
stephanos <github@stephanbehnke.de>
2014-03-02 14:22:15 +0100

    fix test error

--- factory_test.go
+++ factory_test.go
@@ -8,7 +8,6 @@ var _ = Describe("Utility", func() {
 
 	It("return image url", func() {
 		err := New("this is %s", "nice!")
-		Check(err, IsNil)
 		Check(err.Error(), Equals, "this is nice!")
 	})
 })
